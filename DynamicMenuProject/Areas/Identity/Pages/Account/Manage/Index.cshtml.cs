8f4e7163275cfe4278c41c56d952d020e67033e6
Nehal1002 <kanoongonehal@gmail.com>
2021-08-02 09:01:35 +0530

    Add project files.

--- DynamicMenuProject/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ DynamicMenuProject/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using DynamicMenuProject.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -19,15 +20,18 @@ namespace DynamicMenuProject.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IEmailSender _emailSender;
+        private readonly IHostingEnvironment _hostEnvironment;
 
         public IndexModel(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            IHostingEnvironment hostEnvironment)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _emailSender = emailSender;
+            _hostEnvironment = hostEnvironment;
         }
 
         public string Username { get; set; }
@@ -45,6 +49,12 @@ namespace DynamicMenuProject.Areas.Identity.Pages.Account.Manage
             [Required]
             [EmailAddress]
             public string Email { get; set; }
+
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
 
             [Phone]
             [Display(Name = "Phone number")]
@@ -68,13 +78,17 @@ namespace DynamicMenuProject.Areas.Identity.Pages.Account.Manage
             var userName = await _userManager.GetUserNameAsync(user);
             var email = await _userManager.GetEmailAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-
             Username = userName;
-
+            var firstName = user.FirstName;
+            var lastName = user.LastName;
+            var profilePicture = user.ProfilePicture;
             Input = new InputModel
             {
                 Email = email,
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                ProfilePicture = profilePicture,
+                FirstName = firstName,
+                LastName=lastName
             };
 
             IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
@@ -88,20 +102,7 @@ namespace DynamicMenuProject.Areas.Identity.Pages.Account.Manage
             {
                 return Page();
             }
-            //if (Input.ProfilePictureFile != null)
-            //{
-            //    string wwwRootPath = _hostEnvironment.WebRootPath;
-            //    string fileName = Path.GetFileNameWithoutExtension(Input.ProfilePictureFile.FileName);
-            //    string extension = Path.GetExtension(Input.ProfilePictureFile.FileName);
-            //    user.ProfilePicture = DateTime.Now.ToString("yymmssfff") + extension;
-
-
-            //    string path = Path.Combine(wwwRootPath, "Upload", user.ProfilePicture);
-            //    //var mem = new MemoryStream();
-            //    //user.ProfilePicture
-            //    var fileStream = new FileStream(path, FileMode.Create);
-            //    Input.ProfilePictureFile.CopyTo(fileStream);
-            //}
+            
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
@@ -128,6 +129,34 @@ namespace DynamicMenuProject.Areas.Identity.Pages.Account.Manage
                     var userId = await _userManager.GetUserIdAsync(user);
                     throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
                 }
+            }
+
+            var firstName = user.FirstName;
+            var lastName = user.LastName;
+            if (Input.FirstName != firstName)
+            {
+                user.FirstName = Input.FirstName;
+                await _userManager.UpdateAsync(user);
+            }
+            if (Input.LastName != lastName)
+            {
+                user.LastName = Input.LastName;
+                await _userManager.UpdateAsync(user);
+            }
+
+            if (Input.ProfilePictureFile != null)
+            {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(Input.ProfilePictureFile.FileName);
+                string extension = Path.GetExtension(Input.ProfilePictureFile.FileName);
+                user.ProfilePicture = DateTime.Now.ToString("yymmssfff") + extension;
+
+
+                string path = Path.Combine(wwwRootPath, "Upload", user.ProfilePicture);
+                var fileStream = new FileStream(path, FileMode.Create);
+                Input.ProfilePictureFile.CopyTo(fileStream);
+                await _userManager.UpdateAsync(user);
+
             }
 
             await _signInManager.RefreshSignInAsync(user);
